9272f3600dbbbe518424b4b714ef4c33188ee9ae
Yulieth Lubo <yuliethdotcom@gmail.com>
2022-08-05 16:28:36 +0200

    Global state

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -5,7 +5,7 @@ const TransactionList = () => {
     <>
       <h3>Histoy</h3>
       <ul id="list" className="list">
-        <li class="minus"> Cash <span>-$400</span> <button className="delete-btn">x</button></li>
+        <li className="minus"> Cash <span>-$400</span> <button className="delete-btn">x</button></li>
       </ul>
     </>
   );
