8edc151a73e8406bbb3351d7f43824b56f685ddf
Yulieth Lubo <yuliethdotcom@gmail.com>
2022-08-05 16:09:03 +0200

    Transaction list

---
+++ src/components/TransactionList.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const TransactionList = () => {
+  return (
+    <>
+      <h3>Histoy</h3>
+      <ul id="list" className="list">
+        <li class="minus"> Cash <span>-$400</span> <button className="delete-btn">x</button></li>
+      </ul>
+    </>
+  );
+};
+
+export default TransactionList;
