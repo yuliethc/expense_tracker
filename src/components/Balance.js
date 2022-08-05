d81a0ccb9bb6ff5847265e2c24d53e94d986182e
Yulieth Lubo <yuliethdotcom@gmail.com>
2022-08-05 17:10:34 +0200

    Getting balance from global state

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
+import {GlobalContext} from "../context/GoblalState";
 
 const Balance = () => {
+  const {transactions} = useContext(GlobalContext)
+  const amounts = transactions.map(transaction => transaction.amount);
+  const total = amounts.reduce((sum,item)=>(sum+=item),0).toFixed(2)
   return (
     <div>
       <h4>Your balance</h4>
-      <h1 id="balance">$0.00</h1>
+      <h1 id="balance">{total}</h1>
     </div>
   );
 };
